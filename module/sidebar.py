f7a1d6541191a49810f284388bb389a4ae12ddb5
Jia Ming Low <66296475+Jming9638@users.noreply.github.com>
2022-06-06 18:19:05 +0800

    Update

--- module/sidebar.py
+++ module/sidebar.py
@@ -18,4 +18,10 @@ def sidebar(q_total):
                     st.write("Your input number is our of the range.")
             except:
                 st.write("Please input a valid number.")
+                
+        st.subheader("游戏规则：")
+        st.write("1、《海龟汤》是一款游戏，又叫“是否与此无关”，游戏方法非常简单，在任何地方都可以进行。")
+        st.write("2、一开始由出题者给予一个不完整的故事，让猜题者提问各种可能性的问题。")
+        st.write("3、出题者解答这些问题只能说「是」、「不是」或「与此无关」这些答案。")
+        st.write("4、因此猜题者必须在有限的线索中推理出事件的始末，藉由限定性的问答拼凑出故事的全貌。")
             
