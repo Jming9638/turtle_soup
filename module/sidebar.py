f7f4b15205d4ae307244c2d3d475ac20199a66b9
Jia Ming Low <66296475+Jming9638@users.noreply.github.com>
2022-06-06 23:03:56 +0800

    Update sidebar.py

--- module/sidebar.py
+++ module/sidebar.py
@@ -6,18 +6,18 @@ import time
 def sidebar(q_total):
     hash_fn = basehash.base36()
     with st.sidebar:
-#         st.header("Code Generator")
-#         input_num = st.text_input("Please input an integer between 1 - {}.".format(q_total))
-#         if input_num != "":
-#             try:
-#                 num = int(input_num)
-#                 if num <= q_total and num > 0:
-#                     code_num = hash_fn.hash(num-1)
-#                     st.write("Your code: {}".format(code_num))
-#                 else:
-#                     st.write("Your input number is our of the range.")
-#             except:
-#                 st.write("Please input a valid number.")
+        st.header("Code Generator")
+        input_num = st.text_input("Please input an integer between 1 - {}.".format(q_total))
+        if input_num != "":
+            try:
+                num = int(input_num)
+                if num <= q_total and num > 0:
+                    code_num = hash_fn.hash(num-1)
+                    st.write("Your code: {}".format(code_num))
+                else:
+                    st.write("Your input number is our of the range.")
+            except:
+                st.write("Please input a valid number.")
                 
         st.subheader("游戏规则：")
         st.write("1、《海龟汤》是一款游戏，又叫“是否与此无关”，游戏方法非常简单，在任何地方都可以进行。")
